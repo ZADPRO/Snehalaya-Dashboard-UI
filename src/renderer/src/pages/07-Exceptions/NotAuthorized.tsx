c7ca246fa0f3a408525cac7cfdce72522b9fdecf
ionhax-dev <thirukumara.d@zadroit.com>
2025-07-15 09:18:49 +0530

    exception handling - drafted version

--- src/renderer/src/pages/07-Exceptions/NotAuthorized.tsx
+++ src/renderer/src/pages/07-Exceptions/NotAuthorized.tsx
@@ -1,11 +1,22 @@
-import React from 'react';
+import React from 'react'
+import './Exceptions.css'
+import bgImage from '../../assets/exceptions/bg01.jpg'
 
 const NotAuthorized: React.FC = () => {
-    return (
-        <div>
+  return (
+    <div
+      className="internetIssuePage"
+      style={{
+        backgroundImage: `url(${bgImage})`
+      }}
+    >
+      <div className="content">
+        <h1>No Internet Connection</h1>
+        <br />
+        <p>Please check your network and try again.</p>
+      </div>
+    </div>
+  )
+}
 
-        </div>
-    );
-};
-
-export default NotAuthorized;
+export default NotAuthorized
